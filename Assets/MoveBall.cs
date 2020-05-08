7ff8678b29530e217c1e7194a21aafe6557f5369
Gabriel Ullmann <gabriel.ullmann@agenciagh.com.br>
2020-05-07 22:38:56 -0300

    ajustes física

--- Assets/MoveBall.cs
+++ Assets/MoveBall.cs
@@ -6,50 +6,24 @@ using System;
 public class MoveBall : MonoBehaviour
 {
     public GameObject player;
-
-    public float moveDisPerSec = 10;
-    public bool clicked = false;
-    public Vector3 destination;
+    public Vector3 calc;
 
     private void Start()
     {
-        // At first, set the destination to player's position.
-        destination = player.transform.position;
+        //calc = new Camera();
     }
-
     private void Update()
     {
-        if (Input.GetMouseButton(0) || clicked)
+        if (Input.GetMouseButton(0))
         {
             RaycastHit hit;
-            Vector3 targetPos = new Vector3();
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100) && hit.transform.gameObject.tag == "Terrain")
             {
-                // When click on the terreno, record the position.
-                targetPos = hit.point;
-                player.transform.position = Vector3.MoveTowards(player.transform.position, targetPos, moveDisPerSec * Time.deltaTime);
-                clicked = true;
+                var mouseDir = hit.point - player.transform.position;
+                mouseDir = mouseDir.normalized;
+                player.GetComponent<Rigidbody>().AddForce(mouseDir * 10);
             }
-            print(Math.Abs(player.transform.position.y - targetPos.y));
-            if (Math.Abs(player.transform.position.x - targetPos.x) < 0.3f
-                && Math.Abs(player.transform.position.y - targetPos.y) < 0.3f
-                && Math.Abs(player.transform.position.z - targetPos.z) < 0.3f)
-            {
-                clicked = false;
-            }
-
         }
     }
-
-    // Check the mouse left button click
-    //if (Input.GetMouseButtonDown(0))
-    //{
-    //    RaycastHit hit;
-    //    if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100) && hit.transform.gameObject.tag == "Terrain")
-    //    {
-    //        // When click on the terreno, record the position.
-    //        destination = hit.point;
-    //    }
-    //}
 
 }
